dd214ef5b9e95d3f2ceedeef83fc4ceda980af5c
unknown <jacobgoldring@yahoo.com>
2023-10-08 12:08:31 -0400

    c1 permanent upgrade

--- GachaHell.js
+++ GachaHell.js
@@ -85,7 +85,7 @@ var postPublish = () => {
 
 var init = () => {
     currency = theory.createCurrency();
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 6; i++) {
         stars[i] = theory.createCurrency(starNames[i], starNames[i]);
     }
 
