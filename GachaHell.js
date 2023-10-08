2f4d4ba74bbbfeb78b2d1f134b3005975f3885ea
unknown <jacobgoldring@yahoo.com>
2023-10-08 12:43:41 -0400

    c1 permanent upgrade

--- GachaHell.js
+++ GachaHell.js
@@ -120,7 +120,7 @@ var init = () => {
     // c1
     {
         let getDesc = (level) => "c_1=" + getC1(level).toString(0);
-        c1 = theory.createPermanentUpgrade(0, stars[0], new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
+        c1 = theory.createPermanentUpgrade(4, stars[0], new FirstFreeCost(new ExponentialCost(15, Math.log2(2))));
         c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
         c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
     }
