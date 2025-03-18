f5f0f31a0fa135eafd7889e67167ccc1fa97033e
unknown <jacobgoldring@yahoo.com>
2025-03-17 21:17:25 -0400

    Attempt to fix getTau to make it use tenth root of currency

--- GachaHell.js
+++ GachaHell.js
@@ -369,7 +369,7 @@ var getQuaternaryEntries = () => {
 
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
-var getTau = () => Math.pow(currency.value,0.1);//lower this, maybe tenth root
+var getTau = () => BigNumber.pow(currency.value,0.1);
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 //var getC1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
